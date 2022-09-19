acf87d1a50f7ced4873e414ab13e2214bc0d4ccd
Jasmyne Carson <jasmynecarson@gmail.com>
2022-09-18 22:43:37 -1000

    remove speed multiplier code

--- player.gd
+++ player.gd
@@ -3,8 +3,6 @@ extends CharacterBody3D
 class_name Player
 
 const SPEED = 5.0
-var speed_multiplier: float = 1.0
-var rand_speeds: Array = [0.5, 2]
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -23,7 +21,7 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	var speed = SPEED * speed_multiplier
+	var speed = SPEED
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -40,22 +38,7 @@ func _physics_process(delta):
 
 func add_pumpkin():
 	health += 10
-	if health > 50:
-		speed_multiplier = 1
-		$Sprite3d.modulate = Color(255,255,255,255)
-	#$Sprite3d/Label3d.text = str(pumpkins)
 
 func _on_health_timer_timeout():
 	health -= 1
 	$PlayerUI/HBoxContainer/HealthCount.text = str(health)
-
-func _on_movement_variance_timer_timeout():
-	if health < 50:
-		var change = randi_range(0,1)
-		change = 1
-		if change:
-			speed_multiplier = rand_speeds[randi_range(0,1)]
-			if speed_multiplier < 1:
-				$Sprite3d.modulate = Color(0,0,255,255)
-			elif speed_multiplier > 1:
-				$Sprite3d.modulate = Color(255,0,0,255)
